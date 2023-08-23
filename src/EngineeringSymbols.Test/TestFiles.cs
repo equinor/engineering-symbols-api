3fba14c0ddd4c98542d97ce6e4280a68f4f34f67
Lorentz F. Barstad <29151133+lorentzfb@users.noreply.github.com>
2023-08-23 15:15:21 +0200

    feat: fix tests (#3)

--- src/EngineeringSymbols.Test/TestFiles.cs
+++ src/EngineeringSymbols.Test/TestFiles.cs
@@ -5,14 +5,18 @@ namespace EngineeringSymbols.Test;
 public static class TestFilePaths
 {
     private const string Root = "./SvgTestFiles/";
+    
+    private const string ValidPath = Root + "Valid/";
+    private const string InvalidPath = Root + "Invalid/";
+    
     // VALID FILES
-    public const string File1 = Root + "PV003B.svg";
-    public const string File2 = Root + "PT002A_Option1.svg";
+    public const string File1 = ValidPath + "PV003B.svg";
+    public const string File2 = ValidPath + "PT002A_Option1.svg";
     
     // INVALID FILES
-    public const string InvalidRootElement = Root + "INVALID_Root_Element.svg";
-    public const string InvalidConnectors = Root + "PV003B_Invalid_Connector.svg";
-    public const string InvalidViewBox = Root + "PV003B_Invalid_viewbox.svg";
+    public const string InvalidRootElement = InvalidPath + "INVALID_Root_Element.svg";
+    public const string InvalidConnectors = InvalidPath + "PV003B_Invalid_Connector.svg";
+    public const string InvalidViewBox = InvalidPath + "PV003B_Invalid_viewbox.svg";
 
     // INVALID file paths
     public const string AFileThatNotExists = "./the_best.svg";
