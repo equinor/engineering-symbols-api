cc9e01dac4de944c1e9b68afe860f50fe749e4c8
Lorentz F. Barstad <29151133+lorentzfb@users.noreply.github.com>
2023-08-30 15:07:36 +0200

    chore: tabs

--- src/EngineeringSymbols.Api/Endpoints/EndpointHelpers.cs
+++ src/EngineeringSymbols.Api/Endpoints/EndpointHelpers.cs
@@ -10,136 +10,140 @@ namespace EngineeringSymbols.Api.Endpoints;
 
 public static class EndpointHelpers
 {
-    public static TryAsync<string> GetSymbolCreateContentFromRequest(HttpRequest request)
-        => async () =>
-        {
-            var allowedContentTypes = new[] {ContentTypes.Json, ContentTypes.Svg};
+	public static TryAsync<string> GetSymbolCreateContentFromRequest(HttpRequest request)
+		=> async () =>
+		{
+			var allowedContentTypes = new[] { ContentTypes.Json, ContentTypes.Svg };
 
-            if (request.ContentType is null || !allowedContentTypes.Contains(request.ContentType))
-            {
-                return new Result<string>(new ValidationException(
-                    $"Unsupported Content-Type. Expected ${string.Join(" or ", allowedContentTypes)}, but got {request.ContentType}"));
-            }
+			if (request.ContentType is null || !allowedContentTypes.Contains(request.ContentType))
+			{
+				return new Result<string>(new ValidationException(
+					$"Unsupported Content-Type. Expected ${string.Join(" or ", allowedContentTypes)}, but got {request.ContentType}"));
+			}
 
-            const long maxSizeInKiB = 500;
-            var bodySizeFeature = request.HttpContext.Features.Get<IHttpMaxRequestBodySizeFeature>();
-            
-            if (bodySizeFeature is not null)
-            {
-                // NOTE: This will throw BadHttpRequestException 413 on stream.ReadToEndAsync() if body
-                // size is too large
-                bodySizeFeature.MaxRequestBodySize = maxSizeInKiB * 1024;
-            }
-            
-            if (request.ContentLength == null || request.ContentLength / 1024 > maxSizeInKiB)
-            { 
-                return new Result<string>(new ValidationException(
-                    $"Content size is 0 or greater than {maxSizeInKiB} KiB"));
-            }
+			const long maxSizeInKiB = 500;
+			var bodySizeFeature = request.HttpContext.Features.Get<IHttpMaxRequestBodySizeFeature>();
 
-            using var stream = new StreamReader(request.Body);
-            
-            return await stream.ReadToEndAsync();
-        };
-    
-    public static TryAsync<EngineeringSymbolCreateDto> ParseSymbolCreateContent(string? contentType, string? content)
-        => async () =>
-        {
-            if (content is null || contentType is null)
-            {
-                return new Result<EngineeringSymbolCreateDto>(
-                    new ValidationException("Failed to deserialize symbol content."));
-            }
-            
-            Either<Exception, EngineeringSymbolCreateDto> parsedDto =
-                Left(new Exception("Failed to deserialize symbol content"));
+			if (bodySizeFeature is not null)
+			{
+				// NOTE: This will throw BadHttpRequestException 413 on stream.ReadToEndAsync() if body
+				// size is too large
+				bodySizeFeature.MaxRequestBodySize = maxSizeInKiB * 1024;
+			}
 
-            switch (contentType)
-            {
-                case ContentTypes.Json:
-                {
-                    try
-                    {
-                        var obj = JsonConvert.DeserializeObject<EngineeringSymbolCreateDto>(content);
+			if (request.ContentLength == null || request.ContentLength / 1024 > maxSizeInKiB)
+			{
+				return new Result<string>(new ValidationException(
+					$"Content size is 0 or greater than {maxSizeInKiB} KiB"));
+			}
 
-                        if (obj != null)
-                            parsedDto = obj;
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e);
-                        parsedDto = new ValidationException($"Failed to deserialize JSON content.");
-                    }
+			using var stream = new StreamReader(request.Body);
 
-                    break;
-                }
-                case ContentTypes.Svg:
-                    parsedDto = SvgParser.FromString(content)
-                        .Match<Either<Exception, EngineeringSymbolCreateDto>>(
-                            Succ: result =>
-                            {
-                                if (result.ParseErrors.Count > 0)
-                                    return new ValidationException(
-                                        result.ParseErrors.ToDictionary(pair => pair.Key,
-                                            pair => pair.Value.ToArray()));
+			return await stream.ReadToEndAsync();
+		};
 
-                                if (result.EngineeringSymbolSvgParsed == null)
-                                    return new ValidationException("SVG parse error");
+	public static TryAsync<EngineeringSymbolCreateDto> ParseSymbolCreateContent(string? contentType, string? content)
+		=> async () =>
+		{
+			if (content is null || contentType is null)
+			{
+				return new Result<EngineeringSymbolCreateDto>(
+					new ValidationException("Failed to deserialize symbol content."));
+			}
 
-                                return result.EngineeringSymbolSvgParsed.ToCreateDto("");
-                            },
-                            Fail: exception => exception);
-                    break;
-                default:
-                    parsedDto = new ValidationException(
-                        "Failed to deserialize symbol content. Invalid Content-Type");
-                    break;
-            }
-            
-            return parsedDto.Match<Result<EngineeringSymbolCreateDto>>(dto => dto,
-                ex => new Result<EngineeringSymbolCreateDto>(ex));
-        };
+			Either<Exception, EngineeringSymbolCreateDto> parsedDto =
+				Left(new Exception("Failed to deserialize symbol content"));
 
-    public static TryAsync<string> GetRequestBodyAsString(HttpRequest request)
-        => async () =>
-        {
-            using var stream = new StreamReader(request.Body);
-            var content = await stream.ReadToEndAsync();
-            return content;
-        };
-    
-    public static TryAsync<User> GetUserInformation(ClaimsPrincipal claimsPrincipal)
-        => async () =>
-        {
-            var claims = claimsPrincipal.Claims.ToList();
-            
-            var oidString = claims.Find(claim => claim.Type == "http://schemas.microsoft.com/identity/claims/objectidentifier")?.Value;
-            
-            if (!Guid.TryParse(oidString, out var oid))
-            {
-                return new Result<User>(new ValidationException("Failed to determine user id (oid)."));
-            }
+			switch (contentType)
+			{
+				case ContentTypes.Json:
+					{
+						throw new Exception();
 
-            var roles = claims.Where(claim => claim.Type == ClaimTypes.Role)
-                .Select(claim => claim.Value)
-                .ToList();
-            
-            var friendlyName = claims.Find(c => c.Type == "preferred_username")?.Value;
+						try
+						{
+							var obj = JsonConvert.DeserializeObject<EngineeringSymbolCreateDto>(content);
 
-            return new User(oid, roles, friendlyName ?? "");
-        };
+							if (obj != null)
+							{
+								parsedDto = obj;
+							}
+						}
+						catch (Exception e)
+						{
+							Console.WriteLine(e);
+							parsedDto = new ValidationException($"Failed to deserialize JSON content.");
+						}
+
+						break;
+					}
+				case ContentTypes.Svg:
+					parsedDto = SvgParser.FromString(content)
+						.Match<Either<Exception, EngineeringSymbolCreateDto>>(
+							Succ: result =>
+							{
+								if (result.ParseErrors.Count > 0)
+									return new ValidationException(
+										result.ParseErrors.ToDictionary(pair => pair.Key,
+											pair => pair.Value.ToArray()));
+
+								if (result.EngineeringSymbolSvgParsed == null)
+									return new ValidationException("SVG parse error");
+
+								return result.EngineeringSymbolSvgParsed.ToCreateDto("");
+							},
+							Fail: exception => exception);
+					break;
+				default:
+					parsedDto = new ValidationException(
+						"Failed to deserialize symbol content. Invalid Content-Type");
+					break;
+			}
+
+			return parsedDto.Match<Result<EngineeringSymbolCreateDto>>(dto => dto,
+				ex => new Result<EngineeringSymbolCreateDto>(ex));
+		};
+
+	public static TryAsync<string> GetRequestBodyAsString(HttpRequest request)
+		=> async () =>
+		{
+			using var stream = new StreamReader(request.Body);
+			var content = await stream.ReadToEndAsync();
+			return content;
+		};
+
+	public static TryAsync<User> GetUserInformation(ClaimsPrincipal claimsPrincipal)
+		=> async () =>
+		{
+			var claims = claimsPrincipal.Claims.ToList();
+
+			var oidString = claims.Find(claim => claim.Type == "http://schemas.microsoft.com/identity/claims/objectidentifier")?.Value;
+
+			if (!Guid.TryParse(oidString, out var oid))
+			{
+				return new Result<User>(new ValidationException("Failed to determine user id (oid)."));
+			}
+
+			var roles = claims.Where(claim => claim.Type == ClaimTypes.Role)
+				.Select(claim => claim.Value)
+				.ToList();
+
+			var friendlyName = claims.Find(c => c.Type == "preferred_username")?.Value;
+
+			return new User(oid, roles, friendlyName ?? "");
+		};
 
 
-    public static TryAsync<EngineeringSymbolCreateDto> ValidateCreateDto(EngineeringSymbolCreateDto dto)
-        => async () =>
-        {
-            var validationResult = new EngineeringSymbolCreateDtoValidator().Validate(dto);
+	public static TryAsync<EngineeringSymbolCreateDto> ValidateCreateDto(EngineeringSymbolCreateDto dto)
+		=> async () =>
+		{
+			var validationResult = new EngineeringSymbolCreateDtoValidator().Validate(dto);
 
-            return validationResult.IsValid
-                ? dto
-                : new Result<EngineeringSymbolCreateDto>(
-                    new ValidationException(validationResult.ToDictionary()));
-        };
+			return validationResult.IsValid
+				? dto
+				: new Result<EngineeringSymbolCreateDto>(
+					new ValidationException(validationResult.ToDictionary()));
+		};
 
-    //http://schemas.microsoft.com/identity/claims/objectidentifier
+	//http://schemas.microsoft.com/identity/claims/objectidentifier
 }
